7e70ee2589ec50b76c72abf3ba40887468e36c87
bluevitality <inmoonlight@163.com>
2017-11-04 22:34:01 +0800

    Update GRE Tunnel.sh

--- Linux/网络相关/GRE/GRE Tunnel.sh
+++ Linux/网络相关/GRE/GRE Tunnel.sh
@@ -12,10 +12,10 @@
 #----------------------------------------------------------
 
 TUNNEL_NAME="GRE1"
-HOST_A_PUBLIC_ADDRESS=192.168.139.132
-HOST_A_PRIVATA_ADDRESS=192.168.20.1
-HOST_B_PUBLIC_ADDRESS=192.168.139.134
-HOST_B_PRIVATE_ADDRESS=192.168.20.2
+HOST_A_PUBLIC_ADDRESS=192.168.139.132       #A主机公网地址
+HOST_A_PRIVATA_ADDRESS=192.168.20.1         #A主机GRE内网地址
+HOST_B_PUBLIC_ADDRESS=192.168.139.134       #B主机公网地址
+HOST_B_PRIVATE_ADDRESS=192.168.20.2         #B主机GR内网E地址
 
 
 set -e
@@ -29,27 +29,34 @@ fi
 
 #HOST_A
 function A() {
-
+    
+    #添加GRE隧道
     ip tunnel add ${TUNNEL_NAME} mode gre remote ${HOST_B_PUBLIC_ADDRESS} local ${HOST_A_PUBLIC_ADDRESS} ttl 255
     ip link set ${TUNNEL_NAME} up
     ip address add ${HOST_A_PRIVATA_ADDRESS} peer ${HOST_B_PRIVATE_ADDRESS} dev ${TUNNEL_NAME}
+    
+    #开启路由
     sysctl -w net.ipv4.ip_forward=1
+    ! grep -q 'ip_forward' /etc/sysctl.conf && echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf \
+    || sed -i 's/net.ipv4.ip_forward=.*/net.ipv4.ip_forward=1/g' /etc/sysctl.conf 
     sysctl -p
     
 }
 
 #HOST_B
 function B() {
-
+    
+    #添加GRE隧道
     ip tunnel add ${TUNNEL_NAME} mode gre remote ${HOST_A_PUBLIC_ADDRESS} local ${HOST_B_PUBLIC_ADDRESS} ttl 255
     ip link set ${TUNNEL_NAME} up
     ip address add ${HOST_B_PRIVATE_ADDRESS} peer ${HOST_A_PRIVATA_ADDRESS} dev ${TUNNEL_NAME}
+   
+    #开启路由
     sysctl -w net.ipv4.ip_forward=1
+    ! grep -q 'ip_forward' /etc/sysctl.conf && echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf \
+    || sed -i 's/net.ipv4.ip_forward=.*/net.ipv4.ip_forward=1/g' /etc/sysctl.conf 
     sysctl -p
-    
-    #若需指定去往A主机的其他网段，需在B主机添加路由：ip route add 192.168.XX.0/24  via 192.168.20.1 
 }
-
 
 case $1 in
     "a"|"A")
@@ -63,6 +70,8 @@ case $1 in
         echo -e "remove:\n ip link set ${TUNNEL_NAME} down && ip tunnel del ${TUNNEL_NAME}"
         ;;
 esac
+
+#若需指定去往对面主机的其他网段，需在本主机添加路由：ip route add 192.168.XX.0/24  via $HOST_(A|B)_PRIVATE_ADDRESS
 
 exit 0
 
