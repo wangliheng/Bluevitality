9ce3337640bd3e78717faceffbfc37db627d5c71
bluevitality <19450673+bluevitality@users.noreply.github.com>
2017-11-15 22:56:16 +0800

    Update Demo mail.py

--- Python/Flask/Demo mail.py
+++ Python/Flask/Demo mail.py
@@ -4,28 +4,19 @@ import os
 
 app = Flask(__name__)
 app.config.update(
-    DEBUG = True,
-    MAIL_SERVER='smtp.live.com',
-    MAIL_PROT=25,
-    MAIL_USE_TLS = True,
-    MAIL_USE_SSL = False,
-    MAIL_USERNAME = 'example@hotmail.com',
-    MAIL_PASSWORD = '**********',
-    MAIL_DEBUG = True
+    DEBUG = True,MAIL_SERVER='smtp.live.com',MAIL_PROT=25,MAIL_USE_TLS = True,MAIL_USE_SSL = False,MAIL_USERNAME = 'example@hotmail.com',
+    MAIL_PASSWORD = '**********',MAIL_DEBUG = True
 )
 
 mail = Mail(app)
 
 @app.route('/')
 def index():
-# sender 发送方哈，recipients 邮件接收方列表
+    # sender 发送方哈，recipients 邮件接收方列表
     msg = Message("Hi!This is a test ",sender='example@example.com', recipients=['example@example.com'])
-# msg.body 邮件正文 
-    msg.body = "This is a first email"
-# msg.attach 邮件附件添加
-# msg.attach("文件名", "类型", 读取文件）
-    with app.open_resource("F:\2281393651481.jpg") as fp:
-        msg.attach("image.jpg", "image/jpg", fp.read())
+    msg.body = "This is a first email"                      # msg.body 邮件正文 
+    with app.open_resource("F:\2281393651481.jpg") as fp:   # msg.attach 邮件附件添加
+        msg.attach("image.jpg", "image/jpg", fp.read())     # msg.attach("文件名", "类型", 读取文件）
 
     mail.send(msg)
     print "Mail sent"
