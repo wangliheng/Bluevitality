68cd06d23fb4ef4c0f2a719438103f036bf99d3b
bluevitality <19450673+bluevitality@users.noreply.github.com>
2017-11-15 22:54:04 +0800

    Create Demo mail.py

---
+++ Python/Flask/Demo mail.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask.ext.mail import Mail, Message
+import os
+
+app = Flask(__name__)
+app.config.update(
+    DEBUG = True,
+    MAIL_SERVER='smtp.live.com',
+    MAIL_PROT=25,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = 'example@hotmail.com',
+    MAIL_PASSWORD = '**********',
+    MAIL_DEBUG = True
+)
+
+mail = Mail(app)
+
+@app.route('/')
+def index():
+# sender 发送方哈，recipients 邮件接收方列表
+    msg = Message("Hi!This is a test ",sender='example@example.com', recipients=['example@example.com'])
+# msg.body 邮件正文 
+    msg.body = "This is a first email"
+# msg.attach 邮件附件添加
+# msg.attach("文件名", "类型", 读取文件）
+    with app.open_resource("F:\2281393651481.jpg") as fp:
+        msg.attach("image.jpg", "image/jpg", fp.read())
+
+    mail.send(msg)
+    print "Mail sent"
+    return "Sent"
+
+if __name__ == "__main__":
+    app.run()
