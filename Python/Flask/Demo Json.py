42b89ed9e73054f77b31e55d731299e8fe5b40f0
bluevitality <19450673+bluevitality@users.noreply.github.com>
2017-11-15 23:16:11 +0800

    Update Demo Json.py

--- Python/Flask/Demo Json.py
+++ Python/Flask/Demo Json.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
 from flask import Flask,json
 app = Flask(__name__)
 users = ['Linda','Marion5','Race8']
 
 @app.route('/')
 def v_index():
-    return json.dumps(users),200,[('Content-Type','application/json;charset=utf-8')]
+    return json.dumps(users),200,[('Content-Type','application/json;charset=utf-8')]    #返回内容，状态码，报文头
 
 app.run(host='0.0.0.0',port=8080)
+
