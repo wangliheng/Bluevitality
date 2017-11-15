b3742f62baba89ce0ce1ba0654c8b9d260b5c716
bluevitality <19450673+bluevitality@users.noreply.github.com>
2017-11-15 22:41:59 +0800

    Update Flask_SQLAlchemy 1.py

--- Python/Flask/Flask_SQLAlchemy 1.py
+++ Python/Flask/Flask_SQLAlchemy 1.py
@@ -52,4 +52,42 @@ class Role(db.Model):
 #第1个参数表明这个关系的另一端是哪个模型（类）。如果模型类尚未定义可使用字串形式指定。
 #第2个参数backref，将向User类中添加一个role属性，从而定义反向关系。这属性可替代role_id访问Role模型，此时获取的是模型对象而不是外键的值
 
+# Example 3 一对一关系
+# Role表
+class Role(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(80))
+
+# RoleType表
+class Role_type(db.Model):
+    query_class=Common_list_name_Query
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(120))
+
+#一对一，只需要在属性里改变下定义
+# Role表
+class Role(db.Model):
+    role_type_id=db.Column(db.Integer,db.ForeignKey('role_type.id'))
+
+role=db.relationship('Role',backref='role_type',lazy='dynamic', uselist=False)
+
+# Example 4 一对多关系
+# Role表
+class Role(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(80))
+
+# RoleType表
+class Role_type(db.Model):
+    query_class=Common_list_name_Query
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(120))
+
+# 一对多需要在两个表内斗填上相互的关系
+class Role(db.Model):
+    role_type_id=db.Column(db.Integer,db.ForeignKey('role_type.id')) 	# 用于外键的字段，表明这一列的值应该保存指定名称的远程列的值
+
+class Role_type(db.Model):
+    roles=db.relationship('Role',backref='role_type',lazy='dynamic')
+    #第一个参数为对应参照的类User,第二个参数backref表示给关联的数据库模型添加一个属性
 
