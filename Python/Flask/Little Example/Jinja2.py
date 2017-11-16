4bc42a56c3b8f8a7279f3ee960d012ddd01dcbe1
bluevitality <inmoonlight@163.com>
2017-11-16 17:54:32 +0800

    up

--- Python/Flask/Little Example/Jinja2.py
+++ Python/Flask/Little Example/Jinja2.py
@@ -16,3 +16,36 @@ def user(name):
 # 启动服务器
 if __name__ =='__main__':
 	app.run()
+    
+
+""" ---------------------------------------------------------
+模板语法
+    {{ 变量/表达式 }}
+    {% 语法 %}
+    {# 注释 #}
+
+FOR:
+    {% for color in colors: %}  
+        color {{ loop.index }} : {{color}} <br>  
+    {% endfor %}  
+
+MACRO:
+    {% macro render_color(color) -%}  
+        <div>This is color: {{color}} {{ caller() }}</div>  
+    {%- endmacro %}  
+  
+    {% for color in colors: %}  
+        {% call render_color(color) %} render_color_demo  {% endcall %}  
+    {% endfor %} 
+
+    #定义
+    {% macro input(name, value='', type='text', size=20) -%}
+    <input type="{{ type }}" name="{{ name }}" value="{{ value|e }}" size="{{ size }}">
+    {%- endmacro %}
+    
+    #调用
+    <p>{{ input('username') }}</p>
+    <p>{{ input('password', type='password') }}</p>
+    
+    
+"""
