726c2759eec18e23b89118bcf2056ce3dcc98049
bluevitality <19450673+bluevitality@users.noreply.github.com>
2017-11-21 16:05:05 +0800

    Update DR_Real.sh

--- Linux/Cluster/LVS/DR_Real.sh
+++ Linux/Cluster/LVS/DR_Real.sh
@@ -4,10 +4,10 @@ SNS_VIP=10.0.2.245
  
 case "$1" in
 start)
-       ifconfig lo:0 $SNS_VIP netmask 255.255.255.255 broadcast $SNS_VIP
+       ifconfig lo:0 $SNS_VIP netmask 255.255.255.255 broadcast $SNS_VIP #loopbak非本地物理接口
        /sbin/route add -host $SNS_VIP dev lo:0
        echo "1" >/proc/sys/net/ipv4/conf/lo/arp_ignore  #仅响应本接口的ARP（因lo口非本地接口所以实现了对VIP的ARP静默）
-       echo "2" >/proc/sys/net/ipv4/conf/lo/arp_announce #仅相应收到ARP请求的网卡的MAC（实现排除对本机其他接口的MAC响应）
+       echo "2" >/proc/sys/net/ipv4/conf/lo/arp_announce #仅响应收到ARP请求的网卡的MAC（实现排除对本机其他接口的MAC响应）
        echo "1" >/proc/sys/net/ipv4/conf/all/arp_ignore
        echo "2" >/proc/sys/net/ipv4/conf/all/arp_announce
        sysctl -p >/dev/null 2>&1
