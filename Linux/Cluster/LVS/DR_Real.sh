ab37bbc4722e02d2bcafcd228ff5105ab2cff8e3
bluevitality <19450673+bluevitality@users.noreply.github.com>
2017-11-21 16:00:56 +0800

    Update DR_Real.sh

--- Linux/Cluster/LVS/DR_Real.sh
+++ Linux/Cluster/LVS/DR_Real.sh
@@ -6,8 +6,8 @@ case "$1" in
 start)
        ifconfig lo:0 $SNS_VIP netmask 255.255.255.255 broadcast $SNS_VIP
        /sbin/route add -host $SNS_VIP dev lo:0
-       echo "1" >/proc/sys/net/ipv4/conf/lo/arp_ignore
-       echo "2" >/proc/sys/net/ipv4/conf/lo/arp_announce
+       echo "1" >/proc/sys/net/ipv4/conf/lo/arp_ignore  #仅响应本接口的ARP（因lo口非本地接口所以实现了对VIP的ARP静默）
+       echo "2" >/proc/sys/net/ipv4/conf/lo/arp_announce #
        echo "1" >/proc/sys/net/ipv4/conf/all/arp_ignore
        echo "2" >/proc/sys/net/ipv4/conf/all/arp_announce
        sysctl -p >/dev/null 2>&1
