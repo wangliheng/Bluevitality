b82b692f04a509becf78e13c2f2863d3a2572cee
bluevitality <19450673+bluevitality@users.noreply.github.com>
2017-11-21 14:07:52 +0800

    Update LVS_DR_Server.sh

--- Linux/Cluster/LVS/LVS_DR_Server.sh
+++ Linux/Cluster/LVS/LVS_DR_Server.sh
@@ -8,44 +8,44 @@ REALSERVER_IP2=192.168.0.22
 PORT=80
 
 case "$1" in
-start)           
+    start)           
+    
+        /sbin/ifconfig eth0:3 $VIP broadcast $VIP netmask 255.255.255.255 up
+        /sbin/route add -host $VIP dev eth0:3
+    
+        echo 1 > /proc/sys/net/ipv4/ip_forward
+    
+        /sbin/iptables -F
+        /sbin/iptables -Z
+        /sbin/ipvsadm -C
+    
+        /sbin/ipvsadm -A -t $VIP:80 -s wlc
+        /sbin/ipvsadm -a -t $VIP:80 -r $REALSERVER_IP1 -g -w 1
+        /sbin/ipvsadm -a -t $VIP:80 -r $REALSERVER_IP2 -g -w 2
+        /bin/touch /var/lock/subsys/ipvsadm &> /dev/null
+    ;; 
 
-  /sbin/ifconfig eth0:3 $VIP broadcast $VIP netmask 255.255.255.255 up
-  /sbin/route add -host $VIP dev eth0:3
-
-  echo 1 > /proc/sys/net/ipv4/ip_forward
-
-  /sbin/iptables -F
-  /sbin/iptables -Z
-  /sbin/ipvsadm -C
-
-  /sbin/ipvsadm -A -t $VIP:80 -s wlc
-  /sbin/ipvsadm -a -t $VIP:80 -r $REALSERVER_IP1 -g -w 1
-  /sbin/ipvsadm -a -t $VIP:80 -r $REALSERVER_IP2 -g -w 2
-  /bin/touch /var/lock/subsys/ipvsadm &> /dev/null
-;; 
-
-stop)
-
-  echo 0 > /proc/sys/net/ipv4/ip_forward
-  /sbin/ipvsadm -C
-  /sbin/ifconfig eth0:3 down
-  /sbin/route del $VIP
-  
-  /bin/rm -f /var/lock/subsys/ipvsadm
-  
-  echo "ipvs is stopped..."
-;;
-
-status)
-  if [ ! -e /var/lock/subsys/ipvsadm ]; then
-    echo "ipvsadm is stopped ..."
-  else
-    echo "ipvs is running ..."
-    ipvsadm -L -n
-  fi
-;;
-*)
-  echo "Usage: $0 {start|stop|status}"
-;;
+    stop)
+    
+        echo 0 > /proc/sys/net/ipv4/ip_forward
+        /sbin/ipvsadm -C
+        /sbin/ifconfig eth0:3 down
+        /sbin/route del $VIP
+        
+        /bin/rm -f /var/lock/subsys/ipvsadm
+        
+        echo "ipvs is stopped..."
+    ;;
+    
+    status)
+        if [ ! -e /var/lock/subsys/ipvsadm ]; then
+          echo "ipvsadm is stopped ..."
+        else
+          echo "ipvs is running ..."
+          ipvsadm -L -n
+        fi
+    ;;
+    *)
+        echo "Usage: $0 {start|stop|status}"
+    ;;
 esac
